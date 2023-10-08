c57ecdb0a4619bdc902265d4a272b76cf16662d1
Vishwajitsinh <94284617+Vishwajitsinh@users.noreply.github.com>
2023-10-07 20:08:15 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+//Vishwajitsinh Mahida: 301312106
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// routes
+const indexRoute = require('./public/routes/index');
+app.use('/', indexRoute);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+
+app.set('views', path.join(__dirname, 'Views'));
---
+++ public/routes/index.js
@@ -0,0 +1,34 @@
+//Vishwajitsinh Mahida: 301312106
+const express = require('express');
+const router = express.Router();
+
+
+
+// Home Page
+router.get('/', (req, res) => {
+  res.render('home.ejs',{counter:2});
+});
+
+// About Page
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+// Projects Page
+router.get('/projects', (req, res) => {
+  
+  res.render('projects');
+});
+
+// Services Page
+router.get('/services', (req, res) => {
+ 
+  res.render('services');
+});
+
+// Contact Page
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+module.exports = router;
